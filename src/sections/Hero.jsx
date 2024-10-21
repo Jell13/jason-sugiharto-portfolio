6cceb04581c9cefd84e632b54b46af545794ed40
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-10-21 09:02:14 -0700

    added animation where we scroll down it slowly fades away

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,23 +6,31 @@ const Hero = () => {
     const { scrollY } = useScroll()
 
     const opacity = useTransform(
-        scrollY,
-        [0, 300],
-        [1, 0]
+      scrollY,
+      [0, 300],
+      [1, 0]
+    )
+
+    const translate = useTransform(
+      scrollY,
+      [0, 300],
+      [0, 100]
     )
 
   return (
     <motion.section 
     style={{opacity}}
-    className='h-screen relative grainy'>
-      <div className='w-full h-full flex sticky justify-center items-center px-4'>
+    className='h-screen sticky grainy'>
+      <motion.div 
+      style={{y: translate}}
+      className='w-full h-full flex justify-center items-center px-4'>
         <div className='flex flex-col items-center justify-center gap-3'>
             <h1 className='text-4xl tracking-tighter font-semibold md:text-8xl'>JASON SUGIHARTO</h1>
             <div className=' grid-cols-12'>
                 
             </div>
         </div>
-      </div>
+      </motion.div>
     </motion.section>
   )
 }
