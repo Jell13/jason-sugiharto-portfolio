8a09ab4f440f4672f56a0683c95f2465dd91dad1
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-10-23 20:51:09 -0700

    added grid in the hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
       <motion.div 
       style={{y: translate}}
       className='w-full h-full flex justify-center items-center px-10'>
-        <div className='w-full flex flex-col items-center justify-center gap-3'>
+        <div className='w-full flex flex-col items-center justify-center gap-3 mt-20'>
             <div>
               {"JASON SUGIHARTO".split("").map((letter, i) => (
                 <motion.span 
@@ -41,17 +41,22 @@ const Hero = () => {
                   duration: DURATION,
                   delay: STAGGER * i
                 }}
-                className='md:text-8xl text-3xl font-semibold tracking-tighter'>
+                className='md:text-[7rem] text-3xl font-semibold tracking-tighter'>
                   {letter}
                 </motion.span>
               ))}
             </div>
-            <div className='w-full relative flex gap-10 justify-center'>
-              <div className='flex flex-col col-span-4 justify-center items-center'>
+            <div className='w-full mt-10 relative grid grid-cols-12 justify-between'>
+              <div className='flex col-span-4 flex-col justify-center items-center'>
                 <p>I design and develop website</p>
               </div>
-              <div className='w-[300px] mt-10'>
-                <motion.img layoutId='main-image' transition={{ease: [0.17, 0.67, 0.83, 0.67], duration: 1.3}} className='object-contain' src={myHero}/>
+              <div className='col-span-4 flex justify-center items-center'>
+                <motion.img layoutId='main-image' transition={{ease: [0.17, 0.67, 0.83, 0.67], duration: 1.3}} className='object-contain w-[300px]' src={myHero}/>
+              </div>
+              <div className='flex flex-col items-end col-span-4 justify-end'>
+                <div>
+                  <p>Available for work</p>
+                </div>
               </div>
             </div>
         </div>
